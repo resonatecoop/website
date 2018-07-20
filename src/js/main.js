9e457fb8d83f8c736334bd852f236d32f3185c8c
Alex Crane <alexcranedesigner@gmail.com>
2018-07-19 17:56:03 -0700

    menu sticky on scroll, tweaked js scroll functionality, styling on about and nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,59 @@
-var halftone = require('./lib/halftone');
+var halftone = require('./lib/halftone')
 
-var manifesto = require('./modules/manifesto');
+var manifesto = require('./modules/manifesto')
 
 
-halftone();
+halftone()
 
-manifesto();
+manifesto()
 
-// init controller
-//var controller = new ScrollMagic.Controller({addIndicators: true});
-var controller = new ScrollMagic.Controller();
-var homeParagraphsHeight = $("#home-paragraphs").height();
-var actualHeight = homeParagraphsHeight - 280
+// init scrollmagic controller
+var controller = new ScrollMagic.Controller()
 
-// create a scene
+// home page
+function setHomeHeight() {
+  homeParagraphsHeight = $("#home-paragraphs").height()
+  actualHeight = homeParagraphsHeight - 360
+}
+setHomeHeight()
+window.onresize = function() {
+  setHomeHeight()
+}
+function getHomeHeight() {
+  return actualHeight
+}
+
 new ScrollMagic.Scene({
     triggerElement: '#home-paragraphs',
-		duration: actualHeight
+		duration: getHomeHeight
 	})
 	.setPin("#join-sidebar") // pins the element for the the scene's duration
   //.addIndicators()
 	.addTo(controller); // assign the scene to the controller
+
+// main nav stick
+new ScrollMagic.Scene({
+    triggerElement: '#site-wrap',
+		triggerHook: 0,
+    offset: 500
+	})
+  .setClassToggle("#sticky-nav", "sticky")
+	.addTo(controller);
+
+// main nav opacity
+new ScrollMagic.Scene({
+    triggerElement: '#site-wrap',
+		triggerHook: 0,
+    offset: 100
+	})
+  .setClassToggle("#sticky-nav", "mainnav-opacity")
+	.addTo(controller);
+
+// sticky nav opacity
+new ScrollMagic.Scene({
+    triggerElement: '#site-wrap',
+		triggerHook: 0,
+    offset: 700
+	})
+  .setClassToggle("#sticky-nav", "stickynav-opacity")
+	.addTo(controller);
