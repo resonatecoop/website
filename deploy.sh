8beee37f9866d5d6cdf6de95d3ced17637083408
Alex Crane <alexcranedesigner@gmail.com>
2018-08-09 12:11:35 -0700

    added hugo command to deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,9 @@ set -e
 ROOTDIR="/var/www/coop/resonate/root/"
 PUBLICDIR="public/"
 
+hugo
+
 chmod -fR g-w $PUBLICDIR
 chmod -fR o-rwx $PUBLICDIR
 rsync -av --delete-after public/* $ROOTDIR
 chgrp -fR www-data $ROOTDIR
-
