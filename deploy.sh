2697d86060e0dd938870b4e51cb2f796fbbc2507
hugo at hugo-staging <hugo@hugo-staging>
2018-08-09 18:55:23 +0000

    add new deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+ROOTDIR="/var/www/coop/resonate/root/"
+PUBLICDIR="public/"
+
+chmod -fR g-w $PUBLICDIR
+chmod -fR o-rwx $PUBLICDIR
+rsync -av --delete-after public/* $ROOTDIR
+chgrp -fR www-data $ROOTDIR
+
